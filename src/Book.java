93b4f509788bb0b404260773a68ce50ce3293c19
Richie <yourfriend2428@gmail.com>
2025-07-10 10:54:33 +0800

    Tasks can now be created and saved.

--- src/Book.java
+++ src/Book.java
@@ -1,11 +1,13 @@
 import java.io.File;
 import java.io.FileNotFoundException;
+import java.io.FileWriter;
+import java.io.IOException;
 import java.time.LocalDate;
 import java.time.format.DateTimeFormatter;
 import java.util.*;
 
 public class Book {
-    public Dictionary<Integer,List<Task>> entries = new Hashtable<>();
+    public HashMap<Integer,List<Task>> entries = new HashMap<>();
     public int maxPageSize = 10;
 
     public Book(){
@@ -56,11 +58,14 @@ public class Book {
 
     public void fillBook(){
         try{
+            this.entries.clear();
+            this.entries.put(1, new ArrayList<Task>());
+
             File f = new File(Main.filepath);
             Scanner s = new Scanner(f);
             List<String> fileContents = new ArrayList<>();
             String[] entryDetails = new String[5];
-            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("dd-MM-yyyy");
+            DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
             formatter = formatter.withLocale(Locale.ENGLISH);
 
             while (s.hasNextLine()){
@@ -79,6 +84,43 @@ public class Book {
         }
         catch (FileNotFoundException e){
             System.out.println("Tasks.txt was not found!");
+        }
+    }
+
+    public void saveBook(){
+        try{
+            File f = new File(Main.filepath);
+            FileWriter writer = new FileWriter(Main.filepath);
+            int pageNumber = 0;
+            String temp;
+
+            for (int index : this.entries.keySet()){
+                List<Task> page = this.entries.get(index);
+                pageNumber++;
+                int lineNumber = 0;
+
+                for (Task task : page){
+                    lineNumber++;
+                    int id = task.getID();
+                    String body = task.getBody();
+                    Main.Categories category = task.getCategory();
+                    LocalDate deadline = task.getDeadline();
+                    Boolean completed = task.getCompleted();
+
+                    if (lineNumber != 1 || pageNumber != 1){
+                        temp = String.format("%n%s`%s`%s`%s`%s", id, body, category, deadline, completed);
+                    }
+                    else{
+                        temp = String.format("%s`%s`%s`%s`%s", id, body, category, deadline, completed);
+                    }
+                    writer.write(temp);
+                }
+            }
+
+            writer.close();
+        }
+        catch (IOException e){
+            System.out.println("Something went wrong saving the tasks!");
         }
     }
 }
--- src/Main.java
+++ src/Main.java
@@ -1,9 +1,8 @@
 import java.io.File;
 import java.io.IOException;
-import java.util.ArrayList;
-import java.util.InputMismatchException;
-import java.util.List;
-import java.util.Scanner;
+import java.time.LocalDate;
+import java.time.format.DateTimeFormatter;
+import java.util.*;
 
 public class Main {
     public static final String filepath = "C:\\Users\\Richie\\Documents\\Java\\To-Do List Console Version 2\\Tasks.txt";
@@ -42,7 +41,8 @@ public class Main {
                     viewTasks(1);
                     break;
                 case 2:
-                    // Add tasks
+                    createTask();
+                    mainMenu();
                     break;
                 case 3:
                     // Delete tasks
@@ -96,11 +96,151 @@ public class Main {
             if (selection >= 0 && selection <= book.entries.size()){
                 viewTasks(selection);
             }
+            else{
+                System.out.println();
+                System.out.printf("You must enter an integer between 0 and %s!%n", book.entries.size());
+                selectPageToView(currentIndex);
+            }
         }
         catch (NumberFormatException e){
             System.out.println();
-            System.out.printf("You must enter an integer between 0 and %s%n!", book.entries.size());
+            System.out.printf("You must enter an integer between 0 and %s!%n", book.entries.size());
             selectPageToView(currentIndex);
         }
+    }
+
+    private static void createTask(){
+        Scanner s = new Scanner(System.in);
+        DateTimeFormatter formatter = DateTimeFormatter.ofPattern("yyyy-MM-dd");
+        formatter = formatter.withLocale(Locale.ENGLISH);
+
+        String body = "";
+        Categories category;
+        int intDay;
+        int intMonth;
+        int intYear;
+        String day;
+        String month;
+
+        LocalDate deadline;
+
+        System.out.println("=======Task Creation Menu=======");
+        System.out.println();
+        System.out.println("Please note that you are not allowed to use the '`' character in any input!");
+        System.out.println();
+        while (checkValid(body) || body.length() > 25 || body.isEmpty()){  // Get body
+            System.out.print("Enter task name (Maximum 25 characters): ");
+            body = s.nextLine();
+        }
+        categoryLoop:
+        while (true){  // Get category
+            System.out.print("Enter task category (General(G), Important(I), Unimportant(U), Urgent(X)): ");
+            String categorySelection = s.nextLine();
+            switch (categorySelection) {
+                case "G":
+                    category = Categories.General;
+                    break categoryLoop;
+                case "I":
+                    category = Categories.Important;
+                    break categoryLoop;
+                case "U":
+                    category = Categories.Unimportant;
+                    break categoryLoop;
+                case "X":
+                    category = Categories.Urgent;
+                    break categoryLoop;
+                default:
+            }
+        }
+        while (true){
+            try{
+                System.out.print("Enter task year deadline (2025 onwards): ");
+                intYear = s.nextInt();
+                if (intYear >= 2025){
+                    break;
+                }
+            }
+            catch (NumberFormatException _){}
+        }
+        while (true){
+            try{
+                System.out.print("Enter task month deadline (1-12): ");
+                intMonth = s.nextInt();
+                if (intMonth >= 1 && intMonth <= 12){
+                    month = String.valueOf(intMonth);
+                    if (month.length() == 1){
+                        month = "0" + month;
+                    }
+                    break;
+                }
+            }
+            catch (NumberFormatException _){}
+        }
+        while (true){
+            try{
+                if (intYear % 4 == 0 && intMonth == 2){
+                    System.out.print("Enter task day deadline (1-29): ");
+                    intDay = s.nextInt();
+                    if (intDay >= 1 && intDay <= 29){
+                        day = String.valueOf(intDay);
+                        if (day.length() == 1){
+                            day = "0" + day;
+                        }
+                        break;
+                    }
+                }
+                if (intMonth == 2){
+                    System.out.print("Enter task day deadline (1-28): ");
+                    intDay = s.nextInt();
+                    if (intDay >= 1 && intDay <= 28){
+                        day = String.valueOf(intDay);
+                        if (day.length() == 1){
+                            day = "0" + day;
+                        }
+                        break;
+                    }
+                }
+                if (intMonth == 4 || intMonth == 6 || intMonth == 9 || intMonth == 11){
+                    System.out.print("Enter task day deadline (1-30): ");
+                    intDay = s.nextInt();
+                    if (intDay >= 1 && intDay <= 30){
+                        day = String.valueOf(intDay);
+                        if (day.length() == 1){
+                            day = "0" + day;
+                        }
+                        break;
+                    }
+                }
+                if (intMonth == 1 || intMonth == 3 || intMonth == 5 || intMonth == 7 || intMonth == 8 || intMonth == 10 || intMonth == 12){
+                    System.out.print("Enter task day deadline (1-31): ");
+                    intDay = s.nextInt();
+                    if (intDay >= 1 && intDay <= 31){
+                        day = String.valueOf(intDay);
+                        if (day.length() == 1){
+                            day = "0" + day;
+                        }
+                        break;
+                    }
+                }
+            }
+            catch (NumberFormatException _){}
+        }
+
+        String temp = String.format("%s-%s-%s", intYear, month, day);
+        deadline = LocalDate.parse(temp, formatter);
+
+        Task task = new Task(getLatestID(), body, category, deadline, false);
+        book.writeToBook(task);
+        book.saveBook();
+    }
+
+    private static Boolean checkValid(String string){
+        return string.contains("`");
+    }
+
+    private static int getLatestID(){
+        List<Task> tasks = book.entries.get(book.entries.size());
+        Task temp = tasks.getLast();
+        return temp.getID() + 1;
     }
 }
